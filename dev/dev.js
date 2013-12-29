47f7cae12a67e68539b31b7db90567f91b5fddb3
trinity <imandry.c@gmail.com>
2013-12-29 12:26:14 +0800

    fix display of input json data

--- dev/dev.js
+++ dev/dev.js
@@ -15,7 +15,7 @@ var parse = require('../index');
 parse.load(__dirname + '/dev.qs', function (err, f) {
     if (err) throw err;
     console.log('=============DEVTOOL=============');
-    console.log(' IN: ' + JSON.stringify(input));
+    console.log(' IN: [' + JSON.stringify(input) + ']');
     console.log('OUT: ' + JSON.stringify(f([input])));
     console.log('=============PROGRAM=============');
     console.log(f.code);
